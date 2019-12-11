4d9f78c6acf6d942886f62b93c44b48aa0ac2f77
Piotr Czajkowski <sixtyto@gmail.com>
2019-12-11 19:44:00 +0100

    issue solved

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 print("Hello World")
 print("ELLO MORDO")
-x = int(input("Podaj liczbe"))
-if x ==6:
-    print("Brawo zgadles")
-else:
-    print("NOPE")
+while True:
+    x = int(input("Podaj liczbe: "))
+    if x == 69:
+        print("Brawo zgadles.")
+        break
+    else:
+        print(f"NOPE, {'TOO MUCH' if x > 69 else 'TOO LITTLE'}, TRY AGAIN!")
